bfe8cbb65a4b2711bc19ad4c3ffa9dd9efa8abf9
Oscar <oavr.18@gmail.com>
2024-07-16 23:52:55 -0400

    feat: Add 5-base-apparel-coming-soon-page

---
+++ src/landing-projects/5-base-apparel-coming-soon-page/actionForm.ts
@@ -0,0 +1,28 @@
+const actionSubmit: any = (e: SubmitEvent) => {
+  console.log('Entramos');
+  let inputText: any = document.getElementById('inputText');
+  let getSpanError = document.getElementsByClassName(
+    'container-body__paragraph--error-message'
+  )[0];
+  let getIconError = document.getElementsByClassName('icon-error')[0];
+  let getForm = document.getElementsByTagName('form')[0];
+
+  let regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+
+  if (regexEmail.exec(inputText?.value)) {
+    getSpanError.classList.add('hidden');
+    getIconError.classList.add('hidden');
+    getForm.classList.remove('border-error');
+    return true;
+  } else {
+    getSpanError.classList.remove('hidden');
+    getIconError.classList.remove('hidden');
+    getForm.classList.add('border-error');
+    e.preventDefault();
+  }
+};
+
+const formInput = document.getElementById('form');
+formInput?.addEventListener('submit', e => {
+  actionSubmit(e);
+});
