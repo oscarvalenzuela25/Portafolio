134def92fa205403b6aa60cecaa872b19affea53
Oscar Valenzuela <oscar@xepelin.com>
2024-07-26 13:02:57 -0400

    feat: Add changes in 11-fylo-landing-page-with-two-column-layout

---
+++ src/landing-projects/11-fylo-landing-page-with-two-column-layout/handler.js
@@ -0,0 +1,26 @@
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const formBase = document?.getElementById('formBase');
+const inputMessageError = document?.getElementsByClassName(
+  'input-message-error'
+);
+
+const setClass = (elements, newClass, action) => {
+  for (const element of elements) {
+    if (action === 'remove') {
+      element.classList.remove(newClass);
+    } else {
+      element.classList.add(newClass);
+    }
+  }
+};
+
+formBase?.addEventListener('submit', e => {
+  const email = document?.getElementById('formTextInput');
+  if (!emailRegex.test(email.value.trim())) {
+    setClass(inputMessageError, 'hidden', 'remove');
+    e.preventDefault();
+  } else {
+    setClass(inputMessageError, 'hidden', 'add');
+  }
+});
