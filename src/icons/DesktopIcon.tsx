7566d5108eebbfbcc940edfad48f39e2a31c01dd
Oscar <oavr.18@gmail.com>
2024-08-04 01:03:48 -0400

    feat: Add 16-star-rating-component

---
+++ src/icons/DesktopIcon.tsx
@@ -0,0 +1,40 @@
+import React, { type FC } from 'react';
+import { getColor } from '@utils/iconColors';
+import type { IconProps } from './types';
+
+const ChevronDownIcon: FC<IconProps> = ({
+  isLoading,
+  disabled,
+  styles,
+  color,
+}) => {
+  const colorSelected = getColor({
+    isLoading: isLoading,
+    disabled: disabled,
+    styles: styles,
+    color: color,
+  });
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      style={{
+        ...styles,
+        color: colorSelected,
+        width: styles?.width || 20,
+        height: styles?.height || 20,
+      }}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+      />
+    </svg>
+  );
+};
+
+export default ChevronDownIcon;
